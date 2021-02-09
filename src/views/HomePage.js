a412dddf713be9440f243ce5d1b222c83a118bf8
Vladyslav Bondarenko <bvbasys@gmail.com>
2021-02-09 03:25:18 +0200

    useState and useEffect

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,50 +1,45 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import routes from "../routes";
 import style from "./views.module.css";
 
 import * as API from "../api/api";
+window.addEventListener("click", (e) => console.log(e.target));
+function HomePage() {
+  const [movies, setMovies] = useState({ results: [] });
 
-export default class HomePage extends Component {
-  state = {
-    movies: [],
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     API.popularMovie().then((resData) => {
-      console.log(resData);
-      this.setState({ movies: resData.data.results });
+      const fetchData = resData;
+
+      setMovies(fetchData.data);
     });
-  }
+  }, []);
 
-  render() {
-    const { movies } = this.state;
+  return (
+    <div className={style.homepage}>
+      <h2 className={style.heading}>TRENDING TODAY</h2>
+      <ul className={style.list}>
+        {movies.results.map((movie) => (
+          <li key={movie.id} className={style.item}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt=""
+              className={style.poster_search}
+            />
 
-    return (
-      <div className={style.homepage}>
-        <h2 className={style.heading}>TRENDING TODAY</h2>
-        <ul className={style.list}>
-          {movies.map((movie) => (
-            <li key={movie.id} className={style.item}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt=""
-                className={style.poster_search}
-              />
-
-              <Link
-                className={style.link}
-                to={{
-                  pathname: `${routes.movies}/${movie.id}`,
-                  state: { from: this.props.location },
-                }}
-              >
-                {movie.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+            <Link
+              className={style.link}
+              to={{
+                pathname: `${routes.movies}/${movie.id}`,
+              }}
+            >
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+export default HomePage;
